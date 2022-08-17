9e9a21e6d3edc9fa2d0789fa1e2fcaba8164c7fa
AlexRudnyk <alexander.rudnyk@ukr.net>
2022-08-17 08:47:38 +0300

    Добавляет готовую задачу 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -37,9 +37,10 @@ function onModalOpenner(evt) {
   if (!evt.target.classList.contains("gallery__image")) {
     return;
   }
-  const instance = basicLightbox.create(`
-    <img src="https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg" width="800" height="600">
-`);
 
+  console.log("TARGET: ", evt.target);
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}">
+`);
   instance.show();
 }
